541999d6afd1080e5ffe3bd621892bdb6254675e
aarrunyal <aarrunyal@gmail.com>
2024-02-20 10:40:28 -0500

    updating husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,13 +16,13 @@ module.exports = {
 		'react-app',
 		'react-app/jest',
 		'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-		'prettier'
+		'prettier',
 		// 'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 	],
 	plugins: ['react', 'react-hooks'],
 	rules: {
-		// "react/prop-types": "off",
-		// 'prettier/prettier': 0, // disabled the prettier when serving the project
+		'react/prop-types': 'off',
+		'prettier/prettier': 0, // disabled the prettier when serving the project
 		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
 		// e.g. "@typescript-eslint/explicit-function-return-type": "off",
 	},
